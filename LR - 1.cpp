b3e7b4f039fd7581a33bd4f4c84ff2de24cbf859
nTavvka <48122036+nTavvka@users.noreply.github.com>
2019-03-01 14:27:06 +0300

    Add files via upload

---
+++ LR - 1.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <stdio.h>
+
+//using namespace std;
+
+int Get_indent(int rezult);
+void show(int a, int b, int indent);
+
+int main()
+{
+	int numA, numB;
+	printf("input numA: ");
+	scanf_s("%u", &numA);
+	printf("input numB: ");
+	scanf_s("%u", &numB);
+
+	int indent = Get_indent(numA * numB);
+	
+	show(numA, numB, indent);
+	printf("\n%*u ", indent, numA * numB);
+	return 0;
+}
+
+int Get_indent(int rezult)                 //Функция для посчета отступа
+{                                         
+	int indent = 2;
+	do
+	{
+		indent++;
+		rezult /= 10;
+	} while (rezult);
+	return indent;
+}
+
+
+void show(int numA, int numB, int indent)
+{
+	printf("\n%*u", indent, numA);
+	printf("\nx");
+	printf("\n%*u ", indent, numB);
+	printf("\n ---------");
+
+	if (numB / 10)
+	{
+		do
+		{
+			if (numB / 10 == 0) printf("\n%*u", indent, numA* (numB % 10));
+			else printf("\n+%*u", --indent, numA * (numB % 10));
+			numB /= 10;
+		} while (numB);
+		printf("\n ---------");
+	}
+}
